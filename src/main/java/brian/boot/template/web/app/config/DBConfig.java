e194aba8a781bc5da807e652c1efbb281e183254
Brian Heo <allthatjava@yahoo.ca>
2019-09-13 15:52:55 -0400

    Adds multi environment setup by @Profile

--- src/main/java/brian/boot/template/web/app/config/DBConfig.java
+++ src/main/java/brian/boot/template/web/app/config/DBConfig.java
@@ -1,15 +1,18 @@
 package brian.boot.template.web.app.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 public class DBConfig {
@@ -18,25 +21,59 @@ public class DBConfig {
     private Environment env;
 
 //    @ConfigurationProperties(prefix="spring.datasource")
-    private DataSource dataSource() {
+    @Bean
+    @Profile("dev")
+    public DataSource devDataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName( env.getProperty("spring.datasource.driver-class-name") );
-        dataSource.setUrl( env.getProperty("spring.datasource.url") );
-        dataSource.setUsername( env.getProperty("spring.datasource.username") );
-        dataSource.setPassword( env.getProperty("spring.datasource.password") );
+        dataSource.setDriverClassName( env.getProperty("h2.datasource.driver-class-name") );
+        dataSource.setUrl( env.getProperty("h2.datasource.url") );
+        dataSource.setUsername( env.getProperty("h2.datasource.username") );
+        dataSource.setPassword( env.getProperty("h2.datasource.password") );
 
         return dataSource;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory()
+    @Profile("cloud")
+    public DataSource cloudDataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName( env.getProperty("postgres.datasource.driver-class-name") );
+        dataSource.setUrl( env.getProperty("postgres.datasource.url") );
+        dataSource.setUsername( env.getProperty("postgres.datasource.username") );
+        dataSource.setPassword( env.getProperty("postgres.datasource.password") );
+
+        return dataSource;
+    }
+
+    @Bean("entityManagerFactory")
+    @Primary
+    @Profile(value = { "dev", "default"})
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryDev()
     {
         final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
+        em.setDataSource(devDataSource());
         em.setPackagesToScan("brian.boot.template.web.app.domain");
 
         final Properties prop = new Properties();
-        prop.setProperty("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+        prop.setProperty("hibernate.dialect", env.getProperty("h2.jpa.properties.hibernate.dialect"));
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+
+        em.setJpaProperties(prop);
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        return em;
+    }
+    
+    @Bean("entityManagerFactory")
+    @Profile("cloud")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryCloud()
+    {
+        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(cloudDataSource());
+        em.setPackagesToScan("brian.boot.template.web.app.domain");
+
+        final Properties prop = new Properties();
+        prop.setProperty("hibernate.dialect", env.getProperty("postgres.jpa.properties.hibernate.dialect"));
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 
         em.setJpaProperties(prop);
